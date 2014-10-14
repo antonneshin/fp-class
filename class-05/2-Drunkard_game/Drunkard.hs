26106354939dc50a91dcbeef14df556fb4b1506d
antonneshin <antokha333@yandex.ru>
2014-10-15 01:47:05 +0400

    drunk

--- class-05/2-Drunkard_game/Drunkard.hs
+++ class-05/2-Drunkard_game/Drunkard.hs
@@ -7,16 +7,20 @@ module Drunkard where
   учитывая, что всего в колоде 52 карты.
 -}
 
-data Suit
+data Suit = Spades | Clubs | Diamonds | Hearts 
+		deriving (Show, Eq, Ord)
 
-data Value
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten
+		| Jack | Queen | King | Ace 
+		deriving (Show, Eq, Ord) 
 
 data Card = Card Value Suit
+		deriving (Show, Eq, Ord)
 
 -- 2. Определить функцию, проверяющую, что две переданные ей карты одной масти.
 
 sameSuit :: Card -> Card -> Bool
-sameSuit = undefined
+sameSuit (Card _ a ) (Card _ b) = a==b
 
 {-
   3. Определить функцию, проверяющую, что переданная ей первой карта старше второй
@@ -25,7 +29,7 @@ sameSuit = undefined
 -}
 
 beats :: Card -> Card -> Ordering
-c1 `beats` c2 = undefined
+c1 `beats` c2 = compare c1 c2
 
 {-
   4. Определить функцию, которая по паре списков карт возвращает новую пару списков карт
@@ -38,22 +42,47 @@ c1 `beats` c2 = undefined
 -}
 
 game_round :: ([Card], [Card]) -> ([Card], [Card])
-game_round = undefined
+game_round (xs, ys) = foldl(\ (a, b) _ -> my_round (a, b) ) (xs, ys) [1..n]
+ where
+  n = length xs
+
+
+my_round :: Ord a => ([a], [a]) -> ([a], [a])
+my_round (xs, ys)
+  | (compare h1 h2) == LT = (drop 1 xs, (drop 1 ys) ++ [h2] ++ [h1] )
+  | (compare h1 h2) == GT = ((drop 1 xs) ++ [h1] ++ [h2], drop 1 ys  )
+  | otherwise = (drop 1 xs, drop 1 ys)
+  where 
+    h1 = head xs	
+    h2 = head ys
 
 {-
   5. Определить функцию, которая по паре списков возвращает количество раундов, необходимых
   для завершения игры (одна из колод оказывается пустой), и номер победителя.
 -}
 
-data Winner = First | Second
+data Winner = First | Second deriving (Show)
 
 game :: ([Card], [Card]) -> (Winner, Int)
-game = undefined
+game (xs, ys) 
+  | flag == 1 = (First, res)
+  | otherwise = (Second, res)
+  where
+    ls = game_cnt(xs, ys)
+    res = (length ls) - 1 
+    flag = head $ reverse ls
+
+game_cnt :: Num t => ([Card], [Card]) -> [t]
+game_cnt ([], ys) = [-1]
+game_cnt (xs, []) = [1]
+game_cnt (xs, ys) = [0] ++ game_cnt (game_round (xs, ys)) 
 
 {-
   6. Приведите здесь результаты как минимум пяти запусков функции game (в каждом списке
   изначально должно быть не менее 10 карт).
 -}
+--Spades | Clubs | Diamonds | Hearts
+game_test1 = game ([Card Ten Diamonds,Card Seven Clubs, Card Queen Spades, Card Five Hearts, Card King Spades, Card Four Clubs, Card Two Diamonds, Card Three Hearts, Card Jack Spades],[Card Eight Clubs,Card Ace Clubs, Card Nine Diamonds, Card Queen Hearts, Card King Clubs, Card Two Clubs, Card Three Spades, Card Jack Hearts, Card Six Spades, Card Jack Clubs])
 
 {-
   7 (необязательное упражнение). Реализуйте версию функции game, которая помимо результатов
