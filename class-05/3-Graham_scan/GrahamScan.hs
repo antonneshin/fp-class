e21023a3f305285d252bb9e2870410c5a376f03c
antonneshin <antokha333@yandex.ru>
2014-10-17 12:45:46 +0400

    fib

--- class-05/3-Graham_scan/GrahamScan.hs
+++ class-05/3-Graham_scan/GrahamScan.hs
@@ -1,10 +1,20 @@
 {-# LANGUAGE EmptyDataDecls #-}
 
-module GrahamScan where
+import Data.List
+
+--module GrahamScan where
 
 -- 1. Определить тип Point для хранения информации о точке на вещественной плоскости.
 
 data Point = Point (Double, Double) deriving(Show, Ord, Eq)
+
+minus :: Point -> Point -> Point
+Point (ax, ay) `minus` Point (bx, by) = Point (ax-bx, ay-by)
+
+
+plus :: Point -> Point -> Point
+Point (ax, ay) `plus` Point (bx, by) = Point (ax+bx, ay+by)
+
   
 {-
   2. Если заданы три точки a, b, c, можно рассматривать направление поворота от отрезка прямой,
@@ -13,7 +23,7 @@ data Point = Point (Double, Double) deriving(Show, Ord, Eq)
   этих трёх возможностей определить специальный тип Direction.
 -}
 
-data Direction = GoLeft | GoRight | OneLine deriving (Show)
+data Direction = GoLeft | GoRight | OneLine deriving (Show, Eq, Ord)
 
 
 ugol :: (Floating a, Ord a) => a -> a -> a
@@ -25,6 +35,12 @@ ugol x y
   | (x==0)&&(y<0) = -pi/2
   | (x==0)&&(y==0) = 0
 
+
+getx :: Point -> Double
+getx (Point (a, _)) = a
+
+gety :: Point -> Double
+gety (Point (_, b)) = b
 
 direct :: Point -> Point -> Point -> Direction
 direct (Point (ax, ay)) (Point (bx,by)) (Point (cx,cy))
@@ -54,9 +70,41 @@ directions (a:b:c:xs) = [direct a b c] ++ directions (b:c:xs)
   http://www.youtube.com/watch?v=BTgjXwhoMuI
 -}
 
+min_p :: [Point] -> Point
+min_p (h:xs) = foldl(\ m b -> if (gety m) > (gety b) then b else ( if (gety m)==(gety b) then ( if (getx m) > (getx b) then b else m ) else m ) ) h xs
+
+
+polyar_u :: [Point] -> [(Double, Point)]
+polyar_u xs = map (\ h -> (ugol (getx (h)) (gety (h)), h) ) xs
+
+
+
+sortPoint :: [Point] -> [Point]
+sortPoint xs = map (snd) $ sort $ polyar_u xs
+
+
+onestep :: ([Point], (Point, Point)) -> Point -> ([Point], (Point, Point))
+onestep (ls, (pr, t) ) x
+  | (direct pr t x) == GoLeft = (ls++[x],(t,x))
+  | ((direct pr t x) == GoRight) || ((direct pr t x) == OneLine) = (ch_ls++[x], (pr, x))
+  where
+    ch_ls = take ((length ls)-1) ls
+
 graham_scan :: [Point] -> [Point]
-graham_scan = undefined
+graham_scan xs = map (`plus` min_point) $ fst $ foldl (onestep) ([Point (0,0),golova],(Point (0,0),golova)) (spisok)
+  where
+    min_point = min_p xs 
+    sp =map (`minus` min_point) $ delete min_point xs
+    golova = head sp
+    spisok = delete golova sp
+
+
+
 
 {-
   5. Приведите несколько примеров работы функции graham_scan.
 -}
+
+test1 = graham_scan [Point (-2,-2), Point (-2,2), Point (2,-2), Point (2,2), Point (0,-3)]
+
+test2 = drop 1 $ graham_scan $ zipWith (\x y -> Point (x, y)) [0, 0, 0, 1, 2, 2, 3, 5, 5, 3, 0, 2, 2, 3] [0, 0, 1, 0, 0, 0, 0, 3, 3, 5, 2, 2, 3, 2] 
