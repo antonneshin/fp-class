e8935075d987c5e5c5ce3e80b21127f47d6f2ea2
antonneshin <antokha333@yandex.ru>
2014-11-01 01:26:14 +0400

    first

--- class-08/02-listable.hs
+++ class-08/02-listable.hs
@@ -3,10 +3,25 @@
    toList :: a -> [a]
    fromList :: [a] -> a
 -}
+import Data.String
 
+class Listable a where
+  toList :: a -> [a]
+  fromList :: [a] -> a
+
+{-instance Listable Int where
+  toList a = [a]
+  
+  fromList [] = error "List is empty!"
+  fromList (x:_) = x
+-}
 
 {-
   Объявите экземпляры класса типов Listable для следующих типов:
   1) String - строка разбивается по пробелам на список слов.
   2) Integral a - любое целое число разбивается на список цифр.
 -}
+
+instance Listable String where
+  (toList a) :: String = (words a) :: String
+  --toList str = fst $ foldl (\ (list, word) a -> if a==' ' then (list++[word],[]) else (list, word ++ [a])) ([],[]) (str++" ")
