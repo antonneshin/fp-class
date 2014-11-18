a352282e4ef53df9f7920b5095aa8cb412140a71
antonneshin <antokha333@yandex.ru>
2014-11-19 02:24:34 +0400

    всё, что смог

--- class-10/01-data-reader.hs
+++ class-10/01-data-reader.hs
@@ -8,5 +8,22 @@
    (операции >>= и >>, функции ap, liftM и другие функции монадической обработки данных, использование
    блока do не допускается).
 -}
+import System.IO
+import System.Environment
+import Control.Applicative
 
-main = undefined
+type Name = String
+type Age = Int
+type Ngr = String
+data Stud = Stud Name Age Ngr deriving (Show)
+
+makeList :: FilePath -> IO [Stud]
+makeList fname = readFile fname >>= (\ cont -> return $ foldl (\ ls a -> let [n,age,g]=(words a) in ls++[Stud n (read age::Int) g] ) [] (lines cont))
+
+myAction :: [FilePath] -> IO [Stud]
+myAction ls = (++) <$> (makeList $ head ls) <*> (makeList $ last ls)
+
+
+
+main = getArgs>>=(\ ls -> myAction ls)
+
